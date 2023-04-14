c3c9a30e43d8322e2bf8c8636d61049ea1beccfa
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-04-14 17:42:06 +0800

    Added lucky fortune and lucky emoji dynamics

--- src/Fortune/Fortune.js
+++ src/Fortune/Fortune.js
@@ -14,12 +14,26 @@ const Fortune = () => {
   const luckyFortune =
     FORTUNE_LIST[Math.floor(Math.random() * FORTUNE_LIST.length)];
 
-  console.log(luckyFortune);
+  // Put all emoji groups in an array and map for random
+  const selectLuckyEmojis = (emojiGroup) => {
+    return emojiGroup[Math.floor(Math.random() * emojiGroup.length)];
+  };
+  const luckyEmojis = [
+    EMOJI_LIST_ONE,
+    EMOJI_LIST_TWO,
+    EMOJI_LIST_THREE,
+    EMOJI_LIST_FOUR,
+    EMOJI_LIST_FIVE,
+    EMOJI_LIST_SIX,
+  ].map((emojiGroup) => selectLuckyEmojis(emojiGroup));
+
+  console.log(luckyEmojis);
+
   return (
     <div className="fortune">
       <p className="fortune-text">{luckyFortune}</p>
       <p className="fortune-emoji">
-        Lucky Emojis <span>?, ?, ?</span>
+        Lucky Emojis: <span>{luckyEmojis}</span>
       </p>
     </div>
   );
