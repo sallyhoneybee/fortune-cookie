412bb5c3ce9cddc802a34f0815c3ee18a332831f
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-04-14 17:29:55 +0800

    import list of emojis and fortunes

--- src/Fortune/Fortune.js
+++ src/Fortune/Fortune.js
@@ -1,9 +1,23 @@
 import "./Fortune.css";
+import {
+  EMOJI_LIST_ONE,
+  EMOJI_LIST_TWO,
+  EMOJI_LIST_THREE,
+  EMOJI_LIST_FOUR,
+  EMOJI_LIST_FIVE,
+  EMOJI_LIST_SIX,
+} from "./Emoji_list";
+
+import { FORTUNE_LIST } from "./Fortune_list";
 
 const Fortune = () => {
+  const luckyFortune =
+    FORTUNE_LIST[Math.floor(Math.random() * FORTUNE_LIST.length)];
+
+  console.log(luckyFortune);
   return (
     <div className="fortune">
-      <p className="fortune-text">No Duck No Luck</p>
+      <p className="fortune-text">{luckyFortune}</p>
       <p className="fortune-emoji">
         Lucky Emojis <span>?, ?, ?</span>
       </p>
