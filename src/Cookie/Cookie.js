cf66e297d9504db675bf2e0afe814eccad062988
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-04-14 12:16:24 +0800

    Initiated Cookie component and css

---
+++ src/Cookie/Cookie.js
@@ -0,0 +1,24 @@
+import "./Cookie.css";
+import Fortune from "../Fortune/Fortune";
+
+const Cookie = () => {
+  return (
+    <div>
+      <button className="cookie">
+        🥠
+        <div className="cookie-section left"></div>
+        <div className="cookie-section right"></div>
+        <div className="cookie-crumbs">
+          <div className="cookie-crumb"></div>
+          <div className="cookie-crumb"></div>
+          <div className="cookie-crumb"></div>
+          <div className="cookie-crumb"></div>
+          <div className="cookie-crumb"></div>
+        </div>
+        <Fortune />
+      </button>
+    </div>
+  );
+};
+
+export default Cookie;
